ce136f6f9ef902ed8a800011ca16a4df590c9b22
Linh Nguyen <ngtuanlinh.148@gmail.com>
2024-04-18 21:16:47 +0700

    try custom property

--- vipapp/src/main/java/com/nguyenlinhnp/vipapp/rest/FunRestController.java
+++ vipapp/src/main/java/com/nguyenlinhnp/vipapp/rest/FunRestController.java
@@ -1,10 +1,13 @@
 package com.nguyenlinhnp.vipapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${team.name}")
+    private String teamName;
     @GetMapping("/hello")
     public String sayHello(){
         return "hello";
@@ -12,6 +15,8 @@ public class FunRestController {
 
     @GetMapping("/dz")
     public String linhDz(){
-        return "hello adu Linhdz";
+        return "hello adu Linhdz" + teamName;
     }
+
+
 }
